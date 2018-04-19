465efcd41f70c9b4597f20af3a1f7f42b0b3a54e
Ronald Huizer <rhuizer@hexpedition.com>
2018-04-19 03:16:48 -0400

    Accessing the root now works too to show home/
    Signed-off-by: Ronald Huizer <rhuizer@hexpedition.com>

--- goshrine/urls.py
+++ goshrine/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.shortcuts import render
+from home import views
 
 urlpatterns = [
+    re_path('^/?$', views.index),
     path('admin/', admin.site.urls),
     path('home',   include('home.urls')),
     path('login',  include('login.urls')),
