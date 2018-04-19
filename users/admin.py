4d2ae0206d304a463fbf43b43f89ef04fb540f73
Ronald Huizer <rhuizer@hexpedition.com>
2018-04-19 07:03:08 -0400

    Change User admin panel layout.
    Signed-off-by: Ronald Huizer <rhuizer@hexpedition.com>

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+#admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = (
+        'login', 'password', 'email', 'rank', 'location', 'url', 'avatar_pic',
+        'bio', 'click_sounds_flag', 'notice_sounds_flag', 'available', 
+        'administrator', 'created_at', 'room'
+    )
