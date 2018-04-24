438f89086808628dbbfcb29f31bc00c0da258ccd
Ronald Huizer <rhuizer@hexpedition.com>
2018-04-23 22:53:56 -0400

    For now store rooms joined in the database.
    This makes it easier to get things to work, although there's no reason for the
    joined rooms to be database persistent, and it leads to expiration race
    conditions if the websocket close events do not trigger.  Later we can do this
    properly, but that'll be some work.
    
    Signed-off-by: Ronald Huizer <rhuizer@hexpedition.com>

--- events/consumers.py
+++ events/consumers.py
@@ -1,11 +1,25 @@
-import json
-from channels.generic.http import AsyncHttpConsumer
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.db import database_sync_to_async
+from rooms.models import Room
 
 class TestConsumer(AsyncJsonWebsocketConsumer):
+    @database_sync_to_async
+    def set_room(self, user, room):
+        print("SET ROOM", type(user))
+        user.room = room
+        user.save()
+
+    @database_sync_to_async
+    def get_room(self, room):
+        room_id = int(room.split('/')[-1])
+        try:
+            room = Room.objects.get(pk=room_id)
+        except Room.DoesNotExist:
+            return None
+        return room
+
     async def connect(self):
         print('connect')
-
         # Check if the user is logged in, otherwise, disable the chat.
         #if self.scope["user"].is_anonymous:
         #    await self.close()
@@ -43,6 +57,13 @@ class TestConsumer(AsyncJsonWebsocketConsumer):
         return subscription.replace('/', '.')
 
     async def subscribe(self, subscription):
+        room = None
+        if subscription.startswith('/room/'):
+            room = await self.get_room(subscription)
+            # XXX: error
+            if room is None:
+                return None
+
         print("subscribed to '{}'".format(subscription))
         group = self._channel_to_group(subscription)
         self._channels[subscription] = self.channel_handler
@@ -51,7 +72,8 @@ class TestConsumer(AsyncJsonWebsocketConsumer):
             self.channel_name
         )
 
-        user     = self.scope['user']
+        user = self.scope['user']
+        if room is not None: await self.set_room(user, room)
         response = {
             'action'    : 'user_arrive',
             'id'        : user.id,
@@ -74,10 +96,18 @@ class TestConsumer(AsyncJsonWebsocketConsumer):
         })
 
     async def unsubscribe(self, subscription):
+        room = None
+        if subscription.startswith('/room/'):
+            room = await self.get_room(subscription)
+            # XXX: error
+            if room is None:
+                return None
+
         print("unsubscribing from {}".format(subscription))
         group = self._channel_to_group(subscription)
 
-        user     = self.scope['user']
+        user = self.scope['user']
+        if room is not None: await self.set_room(user, None)
         response = {
             'action'    : 'user_leave',
             'id'        : user.id,
@@ -107,55 +137,3 @@ class TestConsumer(AsyncJsonWebsocketConsumer):
 
     async def channel_handler(self, data):
         print(data)
-
-class AsyncLongPollConsumer(AsyncHttpConsumer):
-    async def handle(self, body):
-        body = await self.decode_json(body.decode('utf-8'))
-
-        await self.send_headers(headers=[
-            ("Content-Type", "application/json"),
-        ])
-        # Headers are only sent after the first body event.
-        # Set "more_body" to tell the interface server to not
-        # finish the response yet:
-        await self.send_body(b"", more_body=True)
-        await self.receive(body)
-
-    async def receive(self, data):
-        print("RECV CALLED")
-        pass
-
-    async def sendx(self, data):
-        data = await self.encode_json(data)
-        print('sending:', data)
-        await self.send_body(data, more_body=False)
-
-    @classmethod
-    async def decode_json(cls, text_data):
-        return json.loads(text_data)
-
-    @classmethod
-    async def encode_json(cls, content):
-        return json.dumps(content).encode('utf-8')
-
-class AsyncCometConsumer(AsyncLongPollConsumer):
-    async def receive(self, data):
-        assert(len(data) == 1)
-        data = data[0]
-
-        if data['channel'] == '/meta/handshake':
-            await self.handle_handshake(data)
-        else:
-            print('recv:', data)
-
-    async def handle_handshake(self, data):
-        response = [{
-            'channel'                 : '/meta/handshake',
-            'version'                 : '1.0',
-            'minimumVersion'          : '0.9',
-            'supportedConnectionTypes': ['long-polling'],
-            'advice'                  : {'timeout': 60000, 'interval': 0},
-            'id'                      : data['id']
-        }]
-        print('handshake:', data)
-        await self.sendx(response)
