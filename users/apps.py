1f8f09f2b623db8ecf20e446ce4ea4298bcd694d
Ronald Huizer <rhuizer@hexpedition.com>
2018-04-24 06:28:45 -0400

    On startup clean the users table and get rid of joined rooms.
    This should really be moved to redis or so later, instead of being
    shoved into non-volatile storage.  For now it works.
    
    Signed-off-by: Ronald Huizer <rhuizer@hexpedition.com>

--- users/apps.py
+++ users/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
-
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):
+        from .models import User
+        User.objects.filter(room__isnull=False).update(room=None)
