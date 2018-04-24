a0028c2183e4c1439632f9d40bfba7b684c7cc52
Ronald Huizer <rhuizer@hexpedition.com>
2018-04-24 08:50:58 -0400

    Add error handling to the edit template.
    Signed-off-by: Ronald Huizer <rhuizer@hexpedition.com>

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,13 @@ user_name_map = {
     'login'                     : 'user[login]',
     'email'                     : 'user[email]',
     'password'                  : 'user[password]',
-    'user_password_confirmation': 'user[password_confirmation]'
+    'user_password_confirmation': 'user[password_confirmation]',
+    'location'                  : 'user[location]',
+    'url'                       : 'user[url]',
+    'bio'                       : 'user[bio]',
+    'click_sounds_flag'         : 'user[click_sounds_flag]',
+    'notice_sounds_flag'        : 'user[notice_sounds_flag]',
+    'available'                 : 'user[available]'
 }
 
 # We do not rely on the User model, as the login field can represent either
@@ -29,6 +35,10 @@ class LoginForm(forms.Form):
         field_name = user_name_map.get(field_name, field_name)
         return super().add_prefix(field_name)
 
+#class PasswordField(models.CharField):
+#    default_validators = [
+#        MinLengthValidator(6)
+#    ]
 class UserForm(forms.ModelForm):
     user_password_confirmation = forms.CharField(
         label='Confirm Password',
@@ -78,4 +88,50 @@ class UserForm(forms.ModelForm):
             'login'   : forms.TextInput(attrs={'id': 'user_login', 'size': 30}),
             'email'   : forms.EmailInput(attrs={'id': 'user_email', 'size':30}),
             'password': forms.PasswordInput(attrs={'id': 'user_password', 'size':30})
+        }
+
+class EditForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ''
+
+        self.fields['login'].error_messages = {
+            'unique'    : "Login has already been taken",
+            'required'  : "Login can't be blank",
+            'max_length': "Login is too long (maximum is %(limit_value)s characters)",
+            'min_length': "Login is too short (minimum is %(limit_value)s characters)",
+            'invalid'   : "Login use only letters, numbers, and .-_ please."
+        }
+        self.fields['email'].error_messages    = {
+            'unique'  : "Email has already been taken",
+            'required': "Email needed for registration.",
+            'invalid' : "Email is invalid"
+        }
+
+    def add_prefix(self, field_name):
+        field_name = user_name_map.get(field_name, field_name)
+        return super().add_prefix(field_name)
+
+    class Meta:
+        model   = User
+        fields  = ['login', 'email', 'location', 'url', 'bio',
+                   'click_sounds_flag', 'notice_sounds_flag', 'available']
+        widgets = {
+            'login'   : forms.TextInput(attrs={'id': 'user_login', 'size': 20}),
+            'email'   : forms.EmailInput(attrs={'id': 'user_email', 'size': 20}),
+            'location': forms.TextInput(attrs={'id': 'user_location', 'size': 20}),
+            'url'     : forms.TextInput(attrs={'id': 'user_url', 'size': 20}),
+            'bio'     : forms.TextInput(attrs={'id': 'user_bio', 'size': 20}),
+            'click_sounds_flag': forms.CheckboxInput(attrs={'id': 'user_click_sounds_flag'}),
+            'notice_sounds_flag': forms.CheckboxInput(attrs={'id': 'user_notice_sounds_flag'}),
+            'available': forms.CheckboxInput(attrs={'id': 'user_available'})
+        }
+        labels = {
+            'login'             : 'Username',
+            'email'             : 'Email address',
+            'url'               : 'Website',
+            'bio'               : 'Description',
+            'click_sounds_flag' : 'Audible stone placement',
+            'notice_sounds_flag': 'Play chime on match request',
+            'available'         : 'Let others challenge you?'
         }
