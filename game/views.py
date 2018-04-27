9a34a486461a381a21a61dc9702eb55cb030fc1a
Ronald Huizer <rhuizer@hexpedition.com>
2018-04-27 03:05:14 -0400

    Template out part of the match_create dialog.
    Signed-off-by: Ronald Huizer <rhuizer@hexpedition.com>

--- game/views.py
+++ game/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.decorators.csrf import csrf_exempt
 
+# XXX: called by application.js shit.  Later we need to set the CSRF
+# cookie.
+# https://stackoverflow.com/questions/6506897/csrf-token-missing-or-incorrect-while-post-parameter-via-ajax-in-django
+@csrf_exempt
 def match_create(request):
-    if request.method != 'GET':
+    if request.method != 'POST':
         raise Http404()
 
-    if 'challenged_player_id' not in request.GET:
+    if 'challenged_player_id' not in request.POST:
         raise Http404()
 
-    if 'room_id' not in request.GET:
+    if 'room_id' not in request.POST:
         raise Http404()
 
     return render(request, 'game/match_create.html', {})
