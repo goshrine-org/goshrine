5d372e08fb15b019448086421f437c066dd3e60c
Ronald Huizer <rhuizer@hexpedition.com>
2018-04-27 03:31:53 -0400

    Match proposal dialog is now fully functional.
    Signed-off-by: Ronald Huizer <rhuizer@hexpedition.com>

--- game/views.py
+++ game/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
+from .forms import MatchCreateForm
+from rooms.models import Room
+from users.models import User
 
 # XXX: called by application.js shit.  Later we need to set the CSRF
 # cookie.
@@ -10,10 +13,21 @@ def match_create(request):
     if request.method != 'POST':
         raise Http404()
 
-    if 'challenged_player_id' not in request.POST:
+    form = MatchCreateForm(request.POST)
+    if not form.is_valid():
         raise Http404()
 
-    if 'room_id' not in request.POST:
-        raise Http404()
+    target_user_id = form.cleaned_data['challenged_player_id']
+    room_id        = form.cleaned_data['room_id']
 
-    return render(request, 'game/match_create.html', {})
+    try:
+        target_user = User.objects.get(pk=target_user_id)
+        room        = Room.objects.get(pk=room_id)
+    except (User.DoesNotExist, Room.DoesNotExist):
+       raise Http404()
+
+    context = {
+        'target_user': target_user,
+        'room'       : room
+    }
+    return render(request, 'game/match_create.html', context)
