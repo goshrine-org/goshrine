336fed8ed22ea6a6e37f25ee9caa718eb3be92e7
Ronald Huizer <rhuizer@hexpedition.com>
2019-05-01 13:41:50 +0000

    Show the 11 most recent games on the home screen.
    Signed-off-by: Ronald Huizer <rhuizer@hexpedition.com>

--- home/views.py
+++ home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from game.models import Game
 from common import flash
 
 def index(request):
+    # Get the 11 most recent games from the database and pass them to render.
+    games = Game.objects.filter(room=1).order_by('-started_at')[:11]
+
     flashes = flash.flashes_get(request)
-    return render(request, 'home/index.html', {'flashes': flashes})
+    return render(request, 'home/index.html', {'flashes': flashes, 'games': games})
 
 def about(request):
     flashes = flash.flashes_get(request)
